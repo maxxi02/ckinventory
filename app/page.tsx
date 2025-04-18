ae41839d7beec2a22914da9e429b25f0b3fa4704
maxxi <129664117+maxxi02@users.noreply.github.com>
2025-04-18 22:58:44 +0800

    minor adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -9,20 +9,20 @@ export default async function Home() {
   const session = await getServerSession();
   return (
     <BackgroundLines>
-      <div className="flex items-center justify-evenly z-95 mx-auto w-full max-w-7xl ">
+      <div className="flex items-center justify-evenly z-95 mx-auto px-0 md:px-5  w-full max-w-7xl ">
         {/* DESCRIPTION */}
-        <div className="scale z-95 flex flex-col items-start justify-center w-full h-screen flex-1">
-          <h2 className="bg-clip-text text-transparent text-start bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
+        <div className="scale z-95 flex flex-col items-center md:items-start justify-center w-full h-screen flex-1">
+          <h2 className="bg-clip-text text-transparent text-center md:text-start bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
             <span className="flex items-center">
               <RiBox3Fill
-                className="dark:text-[whitesmoke] text-black "
+                className="dark:text-[whitesmoke] text-black hidden md:block"
                 size={80}
               />
-              CloudKings—
+              CloudKings
             </span>
             Inventory
           </h2>
-          <p className="max-w-xl  text-sm md:text-lg text-neutral-700 dark:text-neutral-400 text-start">
+          <p className="max-w-xl  text-sm md:text-lg text-neutral-700 dark:text-neutral-400 text-center md:text-start">
             Streamline your stock control with our smart inventory management
             system powered by barcode scanning. Track, update, and organize your
             inventory in real-time with speed and accuracy—reducing errors,
