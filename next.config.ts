a8beb920175a47b19303609e32c50b59f0822ee8
maxxi <129664117+maxxi02@users.noreply.github.com>
2025-05-05 22:37:35 +0800

    fixed next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,9 @@ import { NextConfig } from "next";
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
+  webpack: (config) => {
     // Disable webpack cache completely
     config.cache = false;
-
-    // Additional optimization for TensorFlow.js if you're using it
-    if (!isServer) {
-      // Don't bundle large dependencies on the server
-      config.externals = [
-        ...(config.externals || []),
-        { "@tensorflow/tfjs": "tfjs" },
-      ];
-    }
-
     return config;
   },
   // Increase the memory limit for the build process
